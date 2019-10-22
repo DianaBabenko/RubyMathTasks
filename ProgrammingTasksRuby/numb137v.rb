866521e79e4cb5afad224f6eb9ff3ce1b13babbc
DianaBabenko <baben.k.o.diana2017@gmail.com>
2019-10-22 06:10:22 +0300

    add file

--- ProgrammingTasksRuby/numb137v.rb
+++ ProgrammingTasksRuby/numb137v.rb
@@ -1,12 +1,3 @@
-#a
-def task137a(n, a)
-  for i in 1..n
-    puts "a) #{a}"
-    a += a
-  end
-end
-task137a(3, 4)
-
 #v
 def task137v(n, a)
   for i in 1..n
@@ -15,7 +6,3 @@ def task137v(n, a)
   end
 end
 task137v(4, 12)
-
-
-
-
--- ProgrammingTasksRuby/numb52.rb
+++
@@ -1,9 +0,0 @@
-a = gets.i
-b = gets.i
-c = gets.i
-d = gets.i
-s = gets.i
-t = gets.i
-u = gets.i
-
-#if (s != t);
